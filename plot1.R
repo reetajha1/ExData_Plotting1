86b0b29c5b5b5e497dbfe5bce731f8183001228a
reetajha1 <rjha2@travelers.com>
2017-09-01 20:56:13 -0400

    Successful

---
+++ plot1.R
@@ -0,0 +1,18 @@
+
+downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+downloadFile <- "C:/DataSciencetext/JohnHopkins/household_power_consumption.zip"
+householdFile <-"C:/DataSciencetext/JohnHopkins/household_power_consumption/household_power_consumption.txt"
+##
+if (!file.exists(householdFile)) {
+  download.file(downloadURL, downloadFile, method = "curl")
+  unzip(downloadFile, overwrite = T, exdir = "./Data")
+}
+##
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
